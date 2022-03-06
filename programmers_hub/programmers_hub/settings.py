6494c0483e090d85f53c233f59bcbbe1d4120e35
sanarkk <sanya.dimbrovskyi@gmail.com>
2022-03-06 12:58:29 +0200

    Configured PG database

--- programmers_hub/programmers_hub/settings.py
+++ programmers_hub/programmers_hub/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'programmers_hub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'programmers_hub',
+        'USER': 'hub_user',
+        'PASSWORD': 'sashatop2003',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
